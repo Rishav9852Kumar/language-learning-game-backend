73512a65fa10dad6567d92615cffd8291fa54ca2
Rishav Kumar <rishav@whydonate.com>
2023-11-05 17:47:15 +0530

    user api all working

--- src/account/user.js
+++ src/account/user.js
@@ -28,9 +28,10 @@ async function handleRequest(request, env) {
 }
 // ok working
 async function handleGetRequest(request, conn) {
-	const requestBody = await request.json();
-	const email = requestBody.email;
-	console.log(requestBody);
+	// const requestBody = await request.json();
+	// const email = requestBody.email;
+	const url = new URL(request.url);
+	const email = url.searchParams.get('email');
 	const data = await conn.execute('SELECT * FROM Users where UserEmail = ?; ', [email]);
 	if (data.error) {
 		return new Response(data.error, {
@@ -56,59 +57,60 @@ async function handleGetRequest(request, conn) {
 // done working
 async function handlePutRequest(request, conn) {
 	try {
-    const requestBody = await request.json();
-    const email = requestBody.email;
+		const url = new URL(request.url);
+		const email = url.searchParams.get('email');
+		console.log(email);
+		// Check if the user already exists
+		const userExists = await conn.execute('SELECT * FROM Users WHERE UserEmail = ?;', [email]);
 
-    // Check if the user already exists
-    const userExists = await conn.execute('SELECT * FROM Users WHERE UserEmail = ?;', [email]);
+		if (userExists.rows.length > 0) {
+			return new Response('User Already Exists', {
+				headers: { 'content-type': 'text/plain' },
+				status: 404, // Not Found
+			});
+		}
 
-    if (userExists.rows.length > 0) {
-      return new Response('User Already Exists', {
-        headers: { 'content-type': 'text/plain' },
-        status: 404, // Not Found
-      });
-    }
+		// User does not exist, proceed with the insert
+		const newUser = {
+			name: email,
+			email: email,
+			date: new Date(),
+		};
 
-    // User does not exist, proceed with the insert
-    const newUser = {
-      name: requestBody.email,
-      email: requestBody.email,
-      date: new Date(),
-    };
+		const data = await conn.execute('INSERT INTO Users (UserName, UserEmail, RegistrationDate) VALUES (?, ?, ?);', [
+			newUser.name,
+			newUser.email,
+			newUser.date,
+		]);
 
-    const data = await conn.execute(
-      'INSERT INTO Users (UserName, UserEmail, RegistrationDate) VALUES (?, ?, ?);',
-      [newUser.name, newUser.email, newUser.date]
-    );
+		if (data.error) {
+			return new Response(data.error, {
+				headers: { 'content-type': 'text/plain' },
+				status: 404, // Not Found
+			});
+		}
 
-    if (data.error) {
-      return new Response(data.error, {
-        headers: { 'content-type': 'text/plain' },
-        status: 404, // Not Found
-      });
-    }
+		const result = await conn.execute('SELECT * FROM Users WHERE UserEmail = ?;', [newUser.email]);
 
-    const result = await conn.execute('SELECT * FROM Users WHERE UserEmail = ?;', [newUser.email]);
+		if (result.error) {
+			return new Response(data.error, {
+				headers: { 'content-type': 'text/plain' },
+				status: 404, // Not Found
+			});
+		}
 
-    if (result.error) {
-      return new Response(data.error, {
-        headers: { 'content-type': 'text/plain' },
-        status: 404, // Not Found
-      });
-    }
-
-    return new Response(JSON.stringify(data.rows), {
-      status: 200,
-      headers: {
-        'Content-Type': 'application/json',
-      },
-    });
-  } catch (error) {
-    return new Response(error + '\n' + request, {
-      headers: { 'content-type': 'text/plain' },
-      status: 400, // Bad Request
-    });
-  }
+		return new Response(JSON.stringify(data.rows), {
+			status: 200,
+			headers: {
+				'Content-Type': 'application/json',
+			},
+		});
+	} catch (error) {
+		return new Response(error + '\n' + request, {
+			headers: { 'content-type': 'text/plain' },
+			status: 400, // Bad Request
+		});
+	}
 }
 // done checked
 async function handleDeleteRequest(request, conn) {
